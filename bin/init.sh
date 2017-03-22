5565d594801905bf89b4fdfff6a7f0ec6978d345
ecwpz91 <ecwpz91@users.noreply.github.com>
2017-03-22 01:47:37 -0400

    Updated directory reference

--- bin/init.sh
+++ bin/init.sh
@@ -4,7 +4,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-source bin/env.sh
+source $(dirname "${BASH_SOURCE}")/env.sh
 
 # Update Dockerfile image version
 sed -i "s/\($VERSION_REGEX\)$END_QTE_REGEX/\1$VERSION/g" $SRC_FILE_NAME
